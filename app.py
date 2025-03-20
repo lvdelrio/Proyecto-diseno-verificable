4d619868c106cf78a46ee7093c41f6f94a6a886e
Lucas-Maldo <89178081+Lucas-Maldo@users.noreply.github.com>
2025-03-20 18:26:15 -0300

    Agregar rutas a vistas y crud curso

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS
 from dotenv import load_dotenv
 from db import db, init_db, Course
@@ -10,13 +10,11 @@ load_dotenv()
 # Initialize the Flask app
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://localhost:5000"]}})
-
 # Initialize the database
 init_db(app)
 
 app.add_url_rule("/add_user/<name>/<email>", "add_user", userRoutes.add_user)
 
-#iniciar base de datos a  lo maldito
 @app.before_request
 def create_tables():
     db.create_all()
@@ -28,7 +26,12 @@ def home():
 @app.route('/cursos', methods=['GET'])
 def get_cursos():
     cursos = Course.query.all()
-    return render_template("cursos.html", cursos=cursos)
+    return render_template("Cursos/cursos.html", cursos=cursos)
+
+@app.route('/curso/<int:curso_id>')
+def view_curso(curso_id):
+    curso = Course.query.get_or_404(curso_id)
+    return render_template("Cursos/detalle_curso.html", curso=curso)
 
 @app.route('/agregar_curso', methods=['POST'])
 def add_curso():
@@ -41,12 +44,21 @@ def add_curso():
 
     return redirect(url_for("get_cursos"))
 
-@app.route('/borrar_curso/<int:curso_id>', methods=['DELETE'])
+@app.route('/editar_curso/<int:curso_id>', methods=['POST'])
+def edit_curso(curso_id):
+    curso = Course.query.get_or_404(curso_id)
+    curso.name = request.form["name"]
+    curso.description = request.form["description"]
+    db.session.commit()
+    
+    return redirect(url_for("get_cursos"))
+
+@app.route('/borrar_curso/<int:curso_id>', methods=['POST'])
 def delete_curso(curso_id):
     curso = Course.query.get_or_404(curso_id)
     db.session.delete(curso)
     db.session.commit()
-    return jsonify({"message": "Curso deleted"}), 200
+    return redirect(url_for("get_cursos"))
 
 if __name__ == "__main__":
     app.run(debug=True)

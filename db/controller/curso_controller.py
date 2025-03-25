daf92999d616f4795d5b607e445fa953add1bba2
Lucas-Maldo <89178081+Lucas-Maldo@users.noreply.github.com>
2025-03-24 22:37:38 -0300

    Actualziar nomenclatura de cursos y agregar editar y eliminar

---
+++ db/controller/curso_controller.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from ..models.curso import Curso
+
+def crear_curso(db: Session, name: str, description: str):
+    new_course= Curso(nombre=name, descripcion=description)
+    db.add(new_course)
+    db.commit()
+    db.refresh(new_course)
+    return new_course
+
+def get_curso_by_id(db: Session, course_id: int):
+    return db.query(Curso).filter(Curso.id == course_id).first()
+
+def get_all_cursos(db: Session):
+    return db.query(Curso).all()
+
+
+def editar_curso_by_id(db: Session, course_id: int, name: str, description: str):
+    curso = db.query(Curso).filter(Curso.id == course_id).first()
+    if curso:
+        curso.nombre = name
+        curso.descripcion = description
+        db.commit()
+        db.refresh(curso)
+        return curso
+    return None
+
+def eliminar_curso_by_id(db: Session, course_id: int):
+    curso = db.query(Curso).filter(Curso.id == course_id).first()
+    if curso:
+        db.delete(curso)
+        db.commit()
+        return True
+    return False

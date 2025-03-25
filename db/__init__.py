bee42bf24dd5da41c994467e153aa91f7c0a48bb
Lucas-Maldo <89178081+Lucas-Maldo@users.noreply.github.com>
2025-03-25 15:17:53 -0300

    Added a temporary controller and model for profesor

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,6 @@
 from .config import db, init_db
 from .models.alumno import Alumno
 from .models.curso import Curso
+from .models.profesor import Profesor
 
-__all__ = ['db', 'init_db', 'Curso', 'Alumno']
+__all__ = ['db', 'init_db', 'Curso', 'Alumno', 'Profesor']
---
+++ db/models/profesor.py
@@ -0,0 +1,10 @@
+from ..config import db
+class Profesor(db.Model):
+    __tablename__ = 'profesores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return f"<Profesor(id={self.id}, nombre='{self.nombre}')>"

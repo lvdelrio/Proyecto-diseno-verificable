380d97259e1cf94de235f5b6a5bf778b45e21565
AgBallivian <agballivian@miuandes.cl>
2025-03-25 01:49:21 -0300

    Feat: Alumnos Crud (front + back)

--- db/controller/alumno_controller.py
+++ db/controller/alumno_controller.py
@@ -1,8 +1,15 @@
+from datetime import datetime
 from sqlalchemy.orm import Session
 from ..models.alumno import Alumno
 
-def crear_alumno(db: Session, name: str, email: str):
-    new_user = Alumno(nombre=name, email=email)
+def crear_alumno(db: Session, name: str, email: str, fecha_ingreso: str = None):
+    fecha_ingreso_dt = datetime.strptime(fecha_ingreso, '%Y-%m-%d').date() if fecha_ingreso else None
+    
+    new_user = Alumno(
+        nombre=name,
+        email=email,
+        fecha_ingreso=fecha_ingreso_dt
+    )
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
@@ -14,3 +21,23 @@ def get_alumno_by_id(db: Session, user_id: int):
 def get_all_alumnos(db: Session):
     return db.query(Alumno).all()
 
+def edit_alumno_by_id(db: Session, user_id: int, name: str, email: str, fecha_ingreso: str = None):
+    user = db.query(Alumno).filter(Alumno.id == user_id).first()
+    if user:
+        user.nombre = name
+        user.email = email
+        # Update date if provided
+        if fecha_ingreso:
+            user.fecha_ingreso = datetime.strptime(fecha_ingreso, '%Y-%m-%d').date()
+        db.commit()
+        db.refresh(user)
+        return user
+    return None
+
+def delete_alumno_by_id(db: Session, user_id: int):
+    user = db.query(Alumno).filter(Alumno.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return True
+    return False

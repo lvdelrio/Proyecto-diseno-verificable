291bae78f97fafa30025250e0b158b3bf847ebac
Lucas-Maldo <89178081+Lucas-Maldo@users.noreply.github.com>
2025-03-31 19:26:55 -0300

    Agregados los modelos y controller para la clase seccion ademas de unirlos al init

--- db/__init__.py
+++ db/__init__.py
@@ -3,7 +3,7 @@ from .models.alumno import Alumno
 from .models.curso import Curso
 from .models.notas import Notas
 from .models.evaluacion import Evaluacion
-from .models.secciones import Seccion
+from .models.seccion import Seccion
 from .models.profesor import Profesor
 
 __all__ = ['db', 'init_db', 'Curso', 'Alumno', 'Notas', 'Evaluacion', 'Seccion', 'Profesor']
